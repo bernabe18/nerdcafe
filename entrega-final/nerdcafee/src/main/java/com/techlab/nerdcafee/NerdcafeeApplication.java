c95e6dbd3f32b97f12d4e412c7c3ab589ddcc693
Leandro Vallejos <leanvallejos666@gmail.com>
2025-11-19 21:14:41 -0300

    moviendo proyecto final a su carpeta

---
+++ entrega-final/nerdcafee/src/main/java/com/techlab/nerdcafee/NerdcafeeApplication.java
@@ -0,0 +1,13 @@
+package com.techlab.nerdcafee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdcafeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdcafeeApplication.class, args);
+	}
+
+}
---
+++ entrega-final/nerdcafee/src/test/java/com/techlab/nerdcafee/NerdcafeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.nerdcafee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdcafeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
