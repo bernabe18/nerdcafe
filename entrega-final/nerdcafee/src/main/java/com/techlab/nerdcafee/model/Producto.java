ce451e70b50de9b368961f56335f1830e8e6a967
Leandro Vallejos <leanvallejos666@gmail.com>
2025-11-22 19:35:52 -0300

    estructura de archivos

---
+++ entrega-final/nerdcafee/src/main/java/com/techlab/nerdcafee/model/Producto.java
@@ -0,0 +1,34 @@
+package com.techlab.nerdcafee.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String nombre;
+private double precio;
+private String descripcion;
+private String categoria;
+
+
+public Producto(String nombre, double precio, String descripcion, String categoria) {
+	this.nombre = nombre;
+	this.precio = precio;
+	this.descripcion = descripcion;
+	this.categoria = categoria;
+}
+
+public Producto() {}
+}
